f2ad50baa88bf245e5cdfa686ae7b6eb57bcc16d
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-10-05 15:25:14 +0800

    Fixed style

--- lib/facter/apache_vhosts.rb
+++ lib/facter/apache_vhosts.rb
@@ -1,9 +1,9 @@
 Facter.add(:apache_vhosts) do
   confine { Facter.value(:apache_version) }
 
-  @unnamed_vhost_line     = /^(?<ip>.*):(?<port>\d+)\s+(?<name>.*)\s\((?<location>.*)\)/
-  @named_first_line       = /^(?<ip>.*):(?<port>\d+)\s+is\sa.*/
-  @named_subsequent_lines = /^\s+(?<default>(default server)?)(port (?<port>\d+)\snamevhost)?\s(?<name>.*)\s\((?<location>.*:(?<line>\d+))\)/
+  @unnamed_vhost_line     = %r{^(?<ip>.*):(?<port>\d+)\s+(?<name>.*)\s\((?<location>.*)\)}
+  @named_first_line       = %r{^(?<ip>.*):(?<port>\d+)\s+is\sa.*}
+  @named_subsequent_lines = %r{^\s+(?<default>(default server)?)(port (?<port>\d+)\snamevhost)?\s(?<name>.*)\s\((?<location>.*:(?<line>\d+))\)}
 
   def parse(config)
     return_val    = {}
@@ -39,11 +39,11 @@ Facter.add(:apache_vhosts) do
   setcode do
     if Facter::Util::Resolution.which('apachectl')
       apache_config = Facter::Util::Resolution.exec('apachectl -S 2>&1')
-      Facter.debug "Parsing apache config"
+      Facter.debug 'Parsing apache config'
       parse apache_config
     elsif Facter::Util::Resolution.which('apache2ctl')
       apache_config = Facter::Util::Resolution.exec('apache2ctl -S 2>&1')
-      Facter.debug "Parsing apache config"
+      Facter.debug 'Parsing apache config'
       parse apache_config
     end
   end
